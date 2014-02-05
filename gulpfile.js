594ebf279eede0ba71e391fe0de36a8655690c9e
Davi Ferreira <davi.ferreira@corp.globo.com>
2014-02-05 16:58:54 -0200

    Adds gulpfile to the lint directive

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    jshint = require('gulp-jshint'),
-    stylish = require('jshint-stylish');
+        gutil = require('gulp-util'),
+        jshint = require('gulp-jshint'),
+        stylish = require('jshint-stylish');
 
 gulp.task('default', function(){
-  // place code for your default task here
+    // place code for your default task here
 });
 
 gulp.task('lint', function() {
-  gulp.src('./player/js/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
+    gulp.src(['./player/js/*.js', 'Gulpfile.js'])
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
 });
 
 gulp.task('watch', function() {
-  var watcher = gulp.watch('./player/js/*.js', ['lint',]);
-  watcher.on('changed', function(event){
-    console.log('File '+event.path+' was '+event.type+', running tasks...');
-  });
+    var watcher = gulp.watch('./player/js/*.js', ['lint',]);
+    watcher.on('changed', function(event){
+        console.log('File '+event.path+' was '+event.type+', running tasks...');
+    });
 });
