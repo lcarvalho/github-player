3fd826f058fbbeebdb110c66bba825cc63c72a19
Lucas Carvalho <lucascarvalho@gmail.com>
2014-07-21 22:20:22 -0300

    fix watching on gulpfile and add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,30 @@
+/* global console, require, __dirname*/
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
+    watch = require('gulp-watch'),
     jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
     http = require('http'),
     ecstatic = require('ecstatic');
 
-gulp.task('default', function(){
-    http.createServer(
-    ecstatic({ root: __dirname + '/player' })
-    ).listen(8080);
+(function() {
+    'use strict';
 
-    console.log('Listening on :8080');
-});
+    gulp.task('lint', function() {
+        gulp.src(['./player/js/*.js', 'gulpfile.js'])
+            .pipe(watch(function (files) {
+                return files.pipe(jshint('.jshintrc'))
+                       .pipe(jshint.reporter(stylish));
+            }));
+    });
 
-gulp.task('lint', function() {
-    gulp.src(['./player/js/*.js', 'Gulpfile.js'])
-        .pipe(jshint())
-        .pipe(jshint.reporter(stylish));
-});
+    gulp.task('server', function(){
+        http.createServer(
+        ecstatic({ root: __dirname + '/player' })
+        ).listen(8080);
 
-var watcher = gulp.watch('./player/js/*.js', ['lint',]);
-watcher.on('changed', function(event){
-    console.log('File '+event.path+' was '+event.type+', running tasks...');
-});
+        console.log('Listening on :8080');
+    });
+
+    gulp.task('default', ['server', 'lint']);
+
+})();
